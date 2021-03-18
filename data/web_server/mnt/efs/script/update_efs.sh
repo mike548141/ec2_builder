d039b9bfa52cdf25da01d4988aae290ce2133197
Mike Clements <mike@cxi.nz>
2021-03-19 00:32:23 +1300

    Update paths

--- data/web_server/mnt/efs/script/update_efs.sh
+++ data/web_server/mnt/efs/script/update_efs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Author:       Mike Clements, Competitive Edge
-# Version:      0.1.0-20210310
+# Version:      0.1.1-20210319
 # File:         update_efs.sh
 # License:      GNU GPL v3
 # Language:     bash
@@ -22,23 +22,23 @@
 # Import the default variables from the common source
 source /mnt/efs/script/common_variables.sh
 
-cd ~/ec2_builder
+cd ~/builder
 git stash
 git pull
 
-cp ~/ec2_builder/recipe/web_server/mnt/efs/conf/vhost-httpd.conf '/mnt/efs/conf/vhost-httpd.conf'
+cp ~/builder/data/web_server/mnt/efs/conf/vhost-httpd.conf '/mnt/efs/conf/vhost-httpd.conf'
 
 rm --recursive --force /mnt/efs/script/*
-cp --recursive ~/ec2_builder/recipe/web_server/mnt/efs/script/* '/mnt/efs/script/'
+cp --recursive ~/builder/data/web_server/mnt/efs/script/* '/mnt/efs/script/'
 chmod --recursive 0770 /mnt/efs/script/*.sh
 
-cp ~/ec2_builder/recipe/web_server/mnt/efs/vhost/_default_/conf/instance-specific-httpd.conf '/mnt/efs/vhost/_default_/conf/instance-specific-httpd.conf'
-cp ~/ec2_builder/recipe/web_server/mnt/efs/vhost/_default_/conf/instance-specific-php-fpm.conf '/mnt/efs/vhost/_default_/conf/instance-specific-php-fpm.conf'
+cp ~/builder/data/web_server/mnt/efs/vhost/_default_/conf/instance-specific-httpd.conf '/mnt/efs/vhost/_default_/conf/instance-specific-httpd.conf'
+cp ~/builder/data/web_server/mnt/efs/vhost/_default_/conf/instance-specific-php-fpm.conf '/mnt/efs/vhost/_default_/conf/instance-specific-php-fpm.conf'
 
-cp ~/ec2_builder/recipe/web_server/mnt/efs/vhost/cakeit.nz/conf/httpd.conf '/mnt/efs/vhost/cakeit.nz/conf/httpd.conf'
-cp ~/ec2_builder/recipe/web_server/mnt/efs/vhost/cakeit.nz/conf/php-fpm.conf '/mnt/efs/vhost/cakeit.nz/conf/php-fpm.conf'
-cp ~/ec2_builder/recipe/web_server/mnt/efs/vhost/cakeit.nz/conf/pki.conf '/mnt/efs/vhost/cakeit.nz/conf/pki.conf'
+cp ~/builder/data/web_server/mnt/efs/vhost/cakeit.nz/conf/httpd.conf '/mnt/efs/vhost/cakeit.nz/conf/httpd.conf'
+cp ~/builder/data/web_server/mnt/efs/vhost/cakeit.nz/conf/php-fpm.conf '/mnt/efs/vhost/cakeit.nz/conf/php-fpm.conf'
+cp ~/builder/data/web_server/mnt/efs/vhost/cakeit.nz/conf/pki.conf '/mnt/efs/vhost/cakeit.nz/conf/pki.conf'
 
-cp ~/ec2_builder/recipe/web_server/mnt/efs/vhost/example.com/conf/httpd.conf '/mnt/efs/vhost/example.com/conf/httpd.conf'
-cp ~/ec2_builder/recipe/web_server/mnt/efs/vhost/example.com/conf/php-fpm.conf '/mnt/efs/vhost/example.com/conf/php-fpm.conf'
-cp ~/ec2_builder/recipe/web_server/mnt/efs/vhost/example.com/conf/pki.conf '/mnt/efs/vhost/example.com/conf/pki.conf'
+cp ~/builder/data/web_server/mnt/efs/vhost/example.com/conf/httpd.conf '/mnt/efs/vhost/example.com/conf/httpd.conf'
+cp ~/builder/data/web_server/mnt/efs/vhost/example.com/conf/php-fpm.conf '/mnt/efs/vhost/example.com/conf/php-fpm.conf'
+cp ~/builder/data/web_server/mnt/efs/vhost/example.com/conf/pki.conf '/mnt/efs/vhost/example.com/conf/pki.conf'
